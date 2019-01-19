59e80c29135e0764e3fc09e3a8766cf42b1175d1
Nick Pounders <nickpounders@gmail.com>
2019-01-19 16:15:50 -0500

    Comments and print erase
    This comments the code and erases a print statement

--- PythonDictionary.py
+++ PythonDictionary.py
@@ -1,6 +1,10 @@
 import json
 import difflib
 
+#This method takes in a word.  It checks to see if the lowercase version of the word is in the 
+#data.json file.  If the word is found it returns the definitions found.  If the word is not found
+#it checks to see if there are any close mataches and ask the user if that what they mean and return the
+#definition is so.
 def dictionary(word):
     if word.lower() in data:
         return data[word.lower()]
@@ -17,8 +21,8 @@ def dictionary(word):
             else:
                 print("That word is not in the dictionary")
 
+#Enables access to data in json file
 data = json.load(open("data.json"))
-print(data["rain"])
 
 word = input("Please enter a word:\n")
 definition = dictionary(word)
